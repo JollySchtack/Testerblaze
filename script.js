6a05261b1f4fecb01e55e30b5677768af4f0c2ce
JollySchtack <nhotarogan@gmail.com>
2026-03-13 23:42:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2567,13 +2567,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-const tg = window.Telegram.WebApp;
-
-tg.ready();
-tg.expand();                 // optional: expand the webapp
-tg.setHeaderColor('#ffffff'); // black header
-tg.setBackgroundColor('#ffffff'); // optional: make background black too
-
-
 
 
